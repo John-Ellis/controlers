3ba43f84a2133ecaeb1ba309ab8150f468bedb9c
Karol <notkarol@gmail.com>
2017-01-14 10:36:56 -0500

    initial env commit

---
+++ src/env.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import numpy
+
+class Drone:
+
+    # x and y
+    dim = 2
+    pos = np.zeros(dim)
+    vel = np.zeros(dim)
+    acc = np.zeros(dim)
+    wind_vel = np.zeros(dim)
+    
+    def __init__():
